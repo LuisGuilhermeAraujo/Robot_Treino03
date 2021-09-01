57310b5f6607eefcb8a6dcba0f9b902ddf318db0
LuisGuilhermeAraujo <37601432+LuisGuilhermeAraujo@users.noreply.github.com>
2021-08-31 22:17:39 -0300

    new commit

--- Import_Exyon/LerDados.py
+++ Import_Exyon/LerDados.py
@@ -4,13 +4,13 @@ import  pymssql
 import  pandas as pd
 import  xlrd
 
-def FunctionInsertDados(text):
+def function_dados():
 
     df = pd.read_excel("C:\\Users\\luisg\Downloads\\5_CIVEL_EXTRACAO_DADOS_VF002_20210827.xlsx",
                         sheet_name="Table",
                         sep = ';', 
                         encoding = 'utf-8', 
-                        nrows = 10)
+                        nrows = 5)
     return df
 
 
